5bcec989f6dc029d65641bcefaadd9d7be40b9fd
Angel Osalde <angelvg268@gmail.com>
2025-06-03 15:19:57 -0600

    deploy

---
+++ src/lib/data.ts
@@ -0,0 +1,214 @@
+export const personalInfo = {
+  name: "Ángel Mariel Osalde Salazar",
+  location: "Yucatán, México",
+  email: "Angelvg268@gmail.com",
+  github: "https://github.com/Ninthharp8",
+  linkedin: "https://www.linkedin.com/in/angel-osalde-salazar",
+};
+
+export const workExperience = [
+  /*{
+    company: "Chatbyte GmbH",
+    location: "Remote",
+    position: "Software Engineer",
+    period: "Mar 2024 - Present",
+    achievements: [
+      "Designed and implemented business logic for AWS Lambdas using the Serverless Framework, forming the backbone of the application's functionality and ensuring seamless backend operations.",
+      "Built a comprehensive admin panel for managing content, user data, and analytics.",
+      "Contributed to the development and integration of a scalable CMS for managing blog content and other platform data.",
+      "Reduced database costs by introducing materialized views, optimizing query performance and resource usage.",
+      "Drove the integration of a custom affiliate marketing system, enabling seamless tracking and reporting of referrals and user activities.",
+      "Integrated Text-to-Speech (TTS) services for enhanced user experiences, optimizing both frontend and backend systems.",
+      "Collaborated with cross-functional teams to deliver high-quality features, actively reviewing pull requests to ensure code quality, adherence to standards, and efficient implementation.",
+    ],
+  },*/
+  {
+    company: "Unidad de soporte y mantenimiento de equipo de cómputo",
+    location: "Mérida, Yucatán",
+    position: "Practicante",
+    period: "Sep 2023 - Ene 2024",
+    achievements: [
+      "Instalación y configuración de sistemas operativos (Windows, Linux, MacOS).",
+      "Automatización de tareas administrativas utilizando herramientas de Microsoft 365 y Google Workspace.",
+      "Documentación de procedimientos y actividades en operaciones de TI.",
+      "Propuesta e implementación de soluciones TIC para mejorar la eficiencia en la resolución de problemas del sistema.",
+    ],
+  },
+  {
+    company: "Laboratorio de Machine Learning y Visión por Computadora",
+    location: "Mérida, Yucatán",
+    position: "Servicio Social",
+    period: "Sep 2024 - Mar 2025",
+    achievements: [
+      "Contribución al proyecto de inteligencia artificial en el sector salud ofrecido por la Uady, enfocado en mejorar la detección del parásito del chagas mediante redes neuronales de aprendizaje profundo y técnicas de aprendizaje supervisado.",
+      "Entrenamiento y optimización de modelos YOLO en Python para la detección del parásito de Chagas.",
+      "Generación y almacenamiento de estadísticas de entrenamiento y validación del modelo en formato CSV para su posterior análisis.",
+      "Aplicación de técnicas avanzadas de ciencia de datos para interpretar los resultados, identificar patrones y evaluar el desempeño de cada modelo.",
+      "Creación de visualizaciones y gráficos para representar la evolución de las métricas clave y determinar el modelo más eficiente en la detección del parásito."
+    ],
+  },
+];
+
+export const education = [
+  {
+    institution: "Universidad Autónoma de Yucatán",
+    location: "Mérida, Yucatán",
+    degree: "Licenciatura en ciencias de la computación",
+    period: "Sep 2020 - May 2025",
+    achievements: [
+      "Formación en fundamentos de programación, estructuras de datos, bases de datos, redes y sistemas operativos.",
+      "Aplicación práctica de conocimientos mediante proyectos desarrollados en C++, Python (incluyendo aprendizaje automático) y tecnologías web.",
+      "Uso de herramientas como Git, Visual Studio Code, Jupyter Notebook, MySQL y MongoDB durante prácticas y trabajos académicos."
+    ],
+  },
+];
+export const skills = {
+  programmingLanguages: [
+    "TypeScript",
+    "JavaScript",
+    "Python",
+    "C/C++",
+    "Java",
+  ],
+  frontendDevelopment: [
+    "Nextjs",
+    "Reactjs",
+    "Tailwind CSS",
+    "Angular",
+    "bootstrap 5",
+    "HTML",
+    "CSS",
+  ],
+  backendDevelopment: [
+    "Nodejs", 
+    "Expressjs",
+    "Arquitectura MVC"
+  ],
+  databaseAndStorage: [
+    "PostgreSQL",
+    "MongoDB",
+    "MySQL"
+  ],
+  cloudAndDevOps: [
+    "AWS",
+    "Microsoft 365",
+    "Google WorkSpace",
+    "Git",
+    "GitHub",
+  ],
+  toolsAndServices: [
+    "Visual Studio Code",
+    "Postman",
+    "Microsoft Word",
+    "Microsoft Excel",
+    "Google Docs",
+    "Google Sheets",
+    "Google Colab",
+    "JupyterNotebook",
+  ],
+  aiAndMachineLearning: [
+  "Aprendizaje Supervisado",
+  "Aprendizaje no supervisado",
+  "Entrenamiento, Validación y Pruebas",
+  "vision por computadora",
+  "TensorFlow",
+  "scikit-learn",
+  "Pandas",
+  "OpenCV",
+  "Keras",
+  "YOLO",
+  "Pytorch",
+],
+operatingSystems:[
+  "Windows",
+  "Linux",
+],
+lenguajes:[
+  "Ingles (B1)",
+  "Español Nativo",
+]
+};
+
+export const projects = [
+  {
+    title: "Net Zero Carbon Emissions",
+    github: "https://github.com/NinthHarp8",
+    description: [
+      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
+      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
+      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
+      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
+      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+    ],
+  },
+  {
+    title: "Mental Aarog",
+    github: "https://github.com/NinthHarp8",
+    description: [
+      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    ],
+  },
+];
+
+export const awards = [
+{
+  name: "Data Visualization with Python",
+  issuer: "Coursera",
+  date: "Dic 2023",
+  type: "Certificación",
+  position: "Completado"
+},
+{
+  name: "Using Python to Access Web Data",
+  issuer: "Coursera",
+  date: "Diciembre 2023",
+  type: "Certificación",
+  position: "Completado"
+},
+{
+  name: "Introduction to Python Programming",
+  issuer: "Coursera",
+  date: "Diciembre 2023",
+  type: "Certificación",
+  position: "Completado"
+},
+{
+  name: "Introduction to Linear Algebra and Python",
+  issuer: "Coursera",
+  date: "Noviembre 2023",
+  type: "Certificación",
+  position: "Completado"
+},
+{
+  name: " Cisco Networking Academy: Redes de computadora",
+  issuer:"Cisco",
+  date: " Ene 2024 - May 2024",
+  type: "Curso",
+  position: "Completado"
+},
+{
+  name: " Cisco Networking Academy: Fundamentos de Ciberseguridad",
+  issuer:"Cisco",
+  date: " Ene 2024 - May 2024",
+  type: "Curso",
+  position: "Completado"
+},
+{
+  name: " AWS Academy: Cloud Foundations",
+  issuer:"AWS",
+  date: " Ago 2024 - Dic 2024",
+  type: "Curso",
+  position: "Completado"
+},
+{
+  name: " Desarrollo Web Ágil de API y SPA",
+  issuer:"Uady",
+  date: " Ago 2024 - Dic 2024",
+  type: "Curso",
+  position: "Completado"
+},
+];
