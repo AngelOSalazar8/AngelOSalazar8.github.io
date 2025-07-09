e43c410f5460b2c841acfd9ea1eaf61bec5912f2
Angel Osalde <angelvg268@gmail.com>
2025-07-09 15:20:37 -0600

    Actualizacion de datos

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -8,28 +8,31 @@ export const personalInfo = {
 
 export const workExperience = [
    {
-    company: "Laboratorio de Machine Learning y Visión por Computadora",
+    company: "Laboratorio de Machine Learning y Visión por computadora - UADY",
     location: "Mérida, Yucatán",
     position: "Servicio Social",
     period: "Sep 2024 - Mar 2025",
     achievements: [
-      "Contribución al proyecto de inteligencia artificial en el sector salud ofrecido por la Uady, enfocado en mejorar la detección del parásito del chagas mediante redes neuronales de aprendizaje profundo y técnicas de aprendizaje supervisado.",
-      "Entrenamiento y optimización de modelos YOLO en Python para la detección del parásito de Chagas.",
-      "Generación y almacenamiento de estadísticas de entrenamiento y validación del modelo en formato CSV para su posterior análisis.",
+      "Colaboré en un proyecto de inteligencia artificial enfocado en mejorar el análisis de imágenes biomédicas, contribuyendo al desarrollo de soluciones tecnológicas que apoyen el diagnóstico temprano de enfermedades. Principales Responsabilidades :",
+      "Desarrollo y ajuste de modelos de aprendizaje automático para el análisis de imágenes médicas.",
+      "Automatización del procesamiento de datos y generación de estadísticas en formatos reutilizables (CSV).",
       "Aplicación de técnicas avanzadas de ciencia de datos para interpretar los resultados, identificar patrones y evaluar el desempeño de cada modelo.",
-      "Creación de visualizaciones y gráficos para representar la evolución de las métricas clave y determinar el modelo más eficiente en la detección del parásito."
+      "Interpretación de métricas de desempeño de los modelos y elaboración de visualizaciones para facilitar la toma de decisiones.",
+      "Colaboración con investigadores para documentar metodologías y facilitar la implementación en escenarios de pruebas de laboratorio."
     ],
   },
   {
-    company: "Unidad de soporte y mantenimiento de equipo de cómputo",
+    company: "Unidad de soporte y mantenimiento de equipo de cómputo - UADY",
     location: "Mérida, Yucatán",
     position: "Practicante",
     period: "Sep 2023 - Ene 2024",
     achievements: [
-      "Instalación y configuración de sistemas operativos (Windows, Linux, MacOS).",
-      "Automatización de tareas administrativas utilizando herramientas de Microsoft 365 y Google Workspace.",
-      "Documentación de procedimientos y actividades en operaciones de TI.",
-      "Propuesta e implementación de soluciones TIC para mejorar la eficiencia en la resolución de problemas del sistema.",
+     "Apoyé en la modernización de la infraestructura informática de la facultad, integrando soluciones orientadas a la eficiencia operativa y automatización de procesos técnicos y administrativos. Principales Responsabilidades: ",
+      "Desarrollo de soluciones automatizadas utilizando herramientas de Microsoft 365 y Google Workspace.",
+      "Configuración y mantenimiento de sistemas operativos  Windows, Linux.",
+      "Creación y administración de máquinas virtuales para pruebas de software y estandarización de entornos.",
+      "Documentación técnica de procedimientos para mantenimiento, instalación y configuración de equipos y aplicaciones.",
+      "Propuesta de mejoras para procesos internos de soporte, registro y seguimiento de incidencias."
     ],
   },
 ];
@@ -116,70 +119,74 @@ lenguajes:[
 
 export const projects = [
   {
-    title: "Análisis de Modelos para la Clasificación del Parkinson",
+    title: "Clasificación de Parkinson con modelos de aprendizaje supervisado",
     github: "https://github.com/AngelOSalazar8/supervised-learning-parkinson.git",
     image:"https://github.com/AngelOSalazar8/supervised-learning-parkinson/blob/main/results/graphic-comparison-pressicion.png?raw=true",
     tags: ["Python", "Jupyter"],
     description: [
-      "Estudié el comportamiento de distintos modelos de machine learning aplicados a la detección de Parkinson usando datos de voz.",
-      "Comparé algoritmos como SVM, KNN y Random Forest para evaluar su precisión y confiabilidad.",
-      "Analicé métricas como precisión, matriz de confusión y curvas ROC para entender el rendimiento de cada modelo.",
-      "El trabajo me permitió familiarizarme con técnicas de clasificación supervisada y su aplicación en el área médica.",
-      "Proyecto académico realizado en Jupyter con Python y Scikit-learn."
+      "Aplicación de algoritmos de clasificación supervisada para detectar enfermedades a partir de datos biomédicos.",
+      "Comparativa de modelos clásicos (XGBoost, MLP, Random Forest) usando métricas como precisión, matriz de confusión y AUC-ROC.",
+      "Familiarización con flujos de trabajo en Scikit-learn y experimentación científica con datos reales en notebooks.",
     ],
   },
   {
-    title: "YOLO Chagas Analysis",
+    title: "Inteligencia artificial aplicada al diagnóstico microscópico del Trypanosoma cruzi",
     github: "https://github.com/AngelOSalazar8/yolo-chagas-analysis.git",
     image: "https://github.com/AngelOSalazar8/yolo-chagas-analysis/blob/main/results/inferences/trypanosoma3.png?raw=true",
     tags: ["Python", "YOLOv8", "Ultralytics"],
     description: [
-       "Detección avanzada de *Trypanosoma cruzi* utilizando modelos YOLO desde la versión 6 hasta la 10 para diagnóstico rápido.",
-        "Entrenamiento personalizado y validación cruzada para comparar precisión, recall y métricas mAP entre distintas versiones de YOLO.",
-        "Análisis detallado de rendimiento y visualización de resultados con gráficos para optimizar detección en imágenes microscópicas.",
-        "Implementación basada en Ultralytics y PyTorch, integrando técnicas de aprendizaje profundo para mejorar la detección de parásitos.",
-        "Proyecto realizado como servicio social enfocado en contribuir a la salud pública mediante inteligencia artificial aplicada."
+        "Entrenamiento personalizado de modelos YOLO (v6 a v10) aplicados a microscopía digital.",
+        "Evaluación de métricas avanzadas como mAP, precisión y recall para tareas de visión por computadora.",
+        "Experiencia práctica con deep learning aplicado al sector salud, usando herramientas como Ultralytics y PyTorch."
+    ],
+  },
+  {
+    title: "Generación de imágenes sintéticas con  GAN",
+    github: "https://github.com/Ninthharp8/gen-ia-mango-images-using-GAN.git",
+    image: "https://imgs.search.brave.com/F5J5SZJwDtDxTzlVkzvpysZEPWowAAiN-5OwIu660SI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90b3MtcHJlbWl1/bS9tYW5nby1ob2ph/cy1zb2JyZS1mb25k/by1tYWRlcmEtbmVn/cmEtZnJ1dGFzLXRy/b3BpY2FsZXMtdmlz/dGEtc3VwZXJpb3It/ZXNwYWNpby1saWJy/ZS1zdS10ZXh0b18x/ODcxNjYtMTU1MDIu/anBnP3NlbXQ9YWlz/X2h5YnJpZCZ3PTc0/MA",
+    tags: ["Python",  "Jupyter", "TensorFlow","GoogleColab"],
+    description: [
+    "Entrené una red generativa antagónica (GAN) capaz de crear imágenes sintéticas de mangos a partir de ruido aleatorio.",
+    "Exploré la dinámica entre el generador y el discriminador para entender cómo la red mejora con cada época de entrenamiento.",
+    "Analicé visualmente el progreso del modelo, observando cómo las imágenes generadas se volvían más realistas.",
+    "El objetivo principal fue comprender el funcionamiento de las GANs y su aplicación en la generación de contenido visual artificial.",
+    ],
+  },
+  {
+    title: "Dashboard interactivo de ventas de automóviles",
+    github: "https://github.com/AngelOSalazar8/Dash-Auto-Dashboard.git", 
+    image: "https://github.com/AngelOSalazar8/Dash-Auto-Dashboard/blob/main/assets/newplot.png?raw=true", 
+    tags: ["Python", "Dash", "Plotly", "Pandas", "Jupyter"],
+    description: [
+      "Desarrollé una aplicación web interactiva para visualizar estadísticas históricas de ventas de automóviles usando Dash y Plotly.",
+      "Implementé callbacks reactivos para actualizar gráficas dinámicamente con base en la selección del usuario (año o recesión).",
+      "Generé visualizaciones como líneas, barras y pastel para analizar ventas mensuales, tipos de vehículos, gasto en publicidad y desempleo.",
+      "Utilicé Pandas para el procesamiento de datos y agrupaciones por año, mes y tipo de vehículo.",
+      "Este proyecto me permitió integrar análisis de datos y desarrollo web en Python a través de un dashboard funcional y limpio."
     ],
   },
    {
-    title: "Express Mini Proyectos",
+    title: "APIs REST para gestión escolar: proyectos backend con Node.js",
     github: "https://github.com/AngelOSalazar8/express-mini-projects.git",
     image: "https://repository-images.githubusercontent.com/232512685/70107a80-31f8-11ea-9436-66c4dc25c0e2",
     tags: ["Node", "Express", "JavaScript","Java","Html"],
     description: [
-      "Demuestra microservicios modulares orientados a sistemas escolares y gestión alumno-docente.",
-      "Incluye endpoints para alumnos, profesores y control de asistencia con respuestas en formato JSON.",
-      "Integra bases de datos relacionales (MySQL) y no relacionales (MongoDB) para almacenamiento persistente.",
-      "Diseñado para ilustrar conceptos básicos de diseño de APIs REST, manejo de formularios y pruebas externas entre tecnologías.",
-      "Preparado para la nube: expansión planeada hacia despliegue en AWS con Lambda, EC2 y sistemas de mensajería distribuidos.",
+      "Diseño e implementación de APIs RESTful con Express para gestionar alumnos, docentes y asistencias.",
+      "Integración de bases de datos relacionales y no relacionales (MySQL, MongoDB).",
+      "Práctica en modularización, pruebas con herramientas externas, y preparación para despliegue en la nube (AWS)."
     ],
   },
    {
-    title: "Angular Mini Proyectos",
+    title: "Componentes interactivos en Angular: mini proyectos frontend",
     github: "https://github.com/AngelOSalazar8/angular-mini-proyectos.git",
     image: "https://www.unimedia.tech/wp-content/uploads/2023/11/angular-e1700824276714-1200x675.png",
     tags: ["Node", "Angular", "JavaScript","Css","Html","TypeScript"],
     description: [
-      " Desarrollo de mini proyectos para afianzar conceptos de fundamentos de frontend en Angular mediante práctica aplicada.",
-      " Implementación de componentes reutilizables y modulares, favoreciendo una estructura escalable.",
-      "Integración de formularios interactivos, filtrado en tiempo real y estilos condicionales con binding bidireccional y manejo de eventos.",
-      "Simulación de consumo de APIs utilizando Promesas para modelar flujos de datos asincrónicos.",
-      "Cada proyecto fue estructurado de forma independiente para facilitar su comprensión, reutilización y extensión.",
+      "Construcción de componentes reutilizables y arquitectura escalable en Angular.",
+      "Interactividad con formularios, eventos, y consumo simulado de APIs con Promesas.",
+      "Consolidación de buenas prácticas en Angular (data binding, estilos condicionales, estructura modular)."
     ],
-  },
-  /*  {
-    title: "Exploración del Funcionamiento de una GAN",
-    github: "https://github.com/Ninthharp8/gen-ia-mango-images-using-GAN.git",
-    image: "https://github.com/Ninthharp8/gen-ia-mango-images-using-GAN/blob/main/samples/09-gan-mango.jpg?raw=true",
-    tags: ["Python",  "Jupyter"],
-    description: [
-      "Exploré cómo funciona una red generativa antagónica (GAN) entrenando un modelo que genera imágenes de mangos sintéticos.",
-      "Obtuve imágenes cada vez más realistas conforme la red aprendía durante las épocas de entrenamiento.",
-      "El objetivo fue comprender las etapas del entrenamiento y la arquitectura básica de una GAN.",
-      "Este ejercicio me ayudó a visualizar cómo la inteligencia artificial puede generar contenido visual a partir de ruido.",
-      "Proyecto académico realizado con TensorFlow y Google Colab."
-    ],
-  }, */
+  } 
 ];
 
 export const awards = [
